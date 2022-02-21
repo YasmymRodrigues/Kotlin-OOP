e6dc15f6f637c0d028dce2845522a2ed0cb5d7da
learngit <yasmymrdgs@gmail.com>
2022-02-21 18:26:42 +0000

    interfaces

--- src/pt/ulusofona/cm/kotlin/challenge/Pessoa.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/Pessoa.kt
@@ -15,7 +15,7 @@ abstract class Pessoa(nome: String, dataDeNascimento: Date): Movimentavel {
 
     fun comprarVeiculo(veiculo: Veiculo){}
     fun pesquisarVeiculo(identificador: String): Veiculo {
-        return Veiculo("dd")
+        return veiculo
     }
     fun venderVeiculo(identificador: String, comprador:Pessoa){}
     fun moverVeiculoPara(identificador: String, x:Int, y:Int){}
