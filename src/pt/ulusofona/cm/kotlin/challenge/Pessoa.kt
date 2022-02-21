6348b84a963cea58efb9fb851378f5a2fc59f361
learngit <yasmymrdgs@gmail.com>
2022-02-21 17:46:45 +0000

    abstract val

--- src/pt/ulusofona/cm/kotlin/challenge/Pessoa.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/Pessoa.kt
@@ -6,13 +6,13 @@ import javax.xml.crypto.Data
 class Pessoa(nome: String, dataDeNascimento: Date) {
     private val nome : String =""
     private val list = mutableListOf<Veiculo>()
-    val dataDeNascimento : Date
-    val carta : Carta
-    val posicao : Posicao
+    abstract val dataDeNascimento : Date
+    abstract val carta : Carta
+    abstract val posicao : Posicao
 
 
     fun comprarVeiculo(veiculo: Veiculo){}
-    fun pesquisarVeiculo(identificador: String): Veiculo{}
+    fun pesquisarVeiculo(identificador: String): Veiculo{ return Veiculo(nome, posicao, dataDeNascimento )}
     fun venderVeiculo(identificador: String, comprador:Pessoa){}
     fun moverVeiculoPara(identificador: String, x:Int, y:Int){}
     fun temCarta():Boolean{return true}
