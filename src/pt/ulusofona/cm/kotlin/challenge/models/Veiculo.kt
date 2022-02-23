0b1d3cab93fab6ad4a6fe7c6e6a21274261db50a
learngit <yasmymrdgs@gmail.com>
2022-02-23 11:37:29 +0000

    not overriding anymore class Veiculo.kt

--- src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
@@ -4,23 +4,12 @@ import pt.ulusofona.cm.kotlin.challenge.interfaces.Ligavel
 import pt.ulusofona.cm.kotlin.challenge.interfaces.Movimentavel
 import java.util.*
 
-class Veiculo(identificador: String):Movimentavel, Ligavel {
+class Veiculo(identificador: String) {
     val identificador: String = ""
     val posicao: Posicao = TODO()
     val dataDeAquisicao: Date
 
     fun requerCarta(): Boolean{ return false}
-    override fun ligar() {
-        TODO("Not yet implemented")
-    }
-
-    override fun desligar() {
-        TODO("Not yet implemented")
-    }
-
-    override fun estaLigado(): Boolean {
-        TODO("Not yet implemented")
-    }
 
 
 }
