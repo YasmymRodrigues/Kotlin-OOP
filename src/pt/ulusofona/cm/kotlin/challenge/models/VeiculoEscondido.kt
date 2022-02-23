b1c4879635501b60595f665fb9b005cfd05f995d
learngit <yasmymrdgs@gmail.com>
2022-02-23 11:27:22 +0000

    VeiculoEscondido.kt.kt not abstract

--- src/pt/ulusofona/cm/kotlin/challenge/models/VeiculoEscondido.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/VeiculoEscondido.kt
@@ -2,6 +2,9 @@ package pt.ulusofona.cm.kotlin.challenge.models
 
 import pt.ulusofona.cm.kotlin.challenge.interfaces.Ligavel
 import pt.ulusofona.cm.kotlin.challenge.interfaces.Movimentavel
+import java.util.*
 
-abstract class VeiculoEscondido(): Movimentavel, Ligavel {
+class VeiculoEscondido(){
+    val dataDeAquisicao: Date = TODO()
+
 }
