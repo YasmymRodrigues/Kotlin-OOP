f1f77bab125633ee1820235755b1e40095154437
learngit <yasmymrdgs@gmail.com>
2022-02-26 22:18:48 +0000

    hierarchial

--- src/pt/ulusofona/cm/kotlin/challenge/models/Bicicleta.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Bicicleta.kt
@@ -3,15 +3,15 @@ package pt.ulusofona.cm.kotlin.challenge.models
 import pt.ulusofona.cm.kotlin.challenge.interfaces.Movimentavel
 import javax.xml.crypto.Data
 
-abstract class Bicicleta(identificador: String): Veiculo(identificador) {
+abstract class Bicicleta(identificador: String) {
      val x : Int = 0
      val y: Int = 0
 
-     abstract override fun requerCarta(): Boolean
+     abstract fun requerCarta(): Boolean
 
 
      override fun toString(): String {
-          return "Bicicleta | $identificador"
+          return "Bicicleta | "
           //return "Bicicleta | $identificador | $dataDeAquisicao |  $posicao | $x |$y"
      }
 }
