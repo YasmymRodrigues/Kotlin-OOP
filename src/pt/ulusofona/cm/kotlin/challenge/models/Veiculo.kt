a0423bc880c4e7cb1385111852f6ff148339094b
learngit <yasmymrdgs@gmail.com>
2022-02-26 22:04:26 +0000

    hierarchial

--- src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
@@ -3,10 +3,12 @@ package pt.ulusofona.cm.kotlin.challenge.models
 import pt.ulusofona.cm.kotlin.challenge.interfaces.Movimentavel
 import javax.xml.crypto.Data
 
-abstract class Veiculo(val identificador: String): Movimentavel {
+open class Veiculo(val identificador: String): Movimentavel {
 
     open fun requerCarta(): Boolean{ return false}
-
+    override fun moverPara(x: Int, y: Int) {
+        TODO("Not yet implemented")
+    }
 
 
 }
