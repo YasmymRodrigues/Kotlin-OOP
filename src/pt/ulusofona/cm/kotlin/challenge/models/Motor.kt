145401e99b6b30296a02ddff6d4f3adfdcdb6636
learngit <yasmymrdgs@gmail.com>
2022-02-27 20:38:11 +0000

    hierarchial

--- src/pt/ulusofona/cm/kotlin/challenge/models/Motor.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Motor.kt
@@ -2,7 +2,7 @@ package pt.ulusofona.cm.kotlin.challenge.models
 
 import pt.ulusofona.cm.kotlin.challenge.interfaces.Ligavel
 
-class Motor(cavolos: Int, cilindrada: Int): Ligavel {
+class Motor(cavalos: Int, cilindrada: Int): Ligavel {
     val cavalos: Int = 0
     val cilindrada: Int = 0
     val ligado: Boolean = false
--- src/pt/ulusofona/cm/kotlin/challenge/models/VeiculoEscondido.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/VeiculoEscondido.kt
@@ -2,11 +2,6 @@ package pt.ulusofona.cm.kotlin.challenge.models
 
 
 class VeiculoEscondido(var identificador: String)   {
-    fun requerCarta(): Boolean {
-        TODO("Not yet implemented")
-    }
-
-
 
 
 }
