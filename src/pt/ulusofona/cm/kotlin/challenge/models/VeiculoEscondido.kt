a301c9e116180c1e9b6fa7b7bd7ab65fa9eb7b49
learngit <yasmymrdgs@gmail.com>
2022-02-27 20:31:58 +0000

    hierarchial

--- src/pt/ulusofona/cm/kotlin/challenge/models/VeiculoEscondido.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/VeiculoEscondido.kt
@@ -1,8 +1,5 @@
 package pt.ulusofona.cm.kotlin.challenge.models
 
-import pt.ulusofona.cm.kotlin.challenge.interfaces.Ligavel
-import pt.ulusofona.cm.kotlin.challenge.interfaces.Movimentavel
-import javax.xml.crypto.Data
 
 class VeiculoEscondido(var identificador: String)   {
     fun requerCarta(): Boolean {
