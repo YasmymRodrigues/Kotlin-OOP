1fcde3d9e9ba41cc11da05015d0681ca1bfaad72
learngit <yasmymrdgs@gmail.com>
2022-02-27 13:08:23 +0000

    hierarchial

--- src/pt/ulusofona/cm/kotlin/challenge/models/VeiculoEscondido.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/VeiculoEscondido.kt
@@ -5,15 +5,6 @@ import pt.ulusofona.cm.kotlin.challenge.interfaces.Movimentavel
 import javax.xml.crypto.Data
 
 class VeiculoEscondido(var identificador: String) {
-    val dataDeAquisicao: Data = TODO()
-    fun ligar() {
-        TODO("Not yet implemented")
-    }
-
-    //override fun desligar()
-    fun estaLigado(): Boolean {
-        TODO("Not yet implemented")
-    }
 
 
 
