bca870fa827934e7aa56da449000c3b95d0dbbae
learngit <yasmymrdgs@gmail.com>
2022-03-04 19:29:05 +0000

    hierarchial

--- src/pt/ulusofona/cm/kotlin/challenge/models/VeiculoEscondido.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/VeiculoEscondido.kt
@@ -3,11 +3,11 @@ package pt.ulusofona.cm.kotlin.challenge.models
 
 class VeiculoEscondido(override var identificador: String) :Veiculo(identificador) {
     override fun requerCarta(): Boolean {
-        TODO("Not yet implemented")
+        return true;
     }
 
     override fun ligar() {
-        TODO("Not yet implemented")
+
     }
 
     override fun desligar() {
